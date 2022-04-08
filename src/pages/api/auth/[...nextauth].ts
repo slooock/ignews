2d7d3e172bc549e86e7314df24bec91ed2c82b1f
Kayque Avelar <kayque.avelar@gmail.com>
2022-04-08 16:25:08 -0300

    criando usuario fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,6 +16,17 @@ export default NextAuth({
         },
       },
     }),
-    // ...add more providers here
   ],
+
+  // secret: process.env.JWT_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
